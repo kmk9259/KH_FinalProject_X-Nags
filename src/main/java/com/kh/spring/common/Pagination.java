1a8ccf85f38c8dca39b5cf928977d794c846045b
jinsuk1 <98982398@naver.com>
2021-10-26 20:33:54 +0900

    10-26홍진석

--- src/main/java/com/kh/spring/common/Pagination.java
+++ src/main/java/com/kh/spring/common/Pagination.java
@@ -1,6 +1,6 @@
 package com.kh.spring.common;
 
-import com.kh.spring.board.model.vo.PageInfo;
+import com.kh.spring.community.model.vo.PageInfo;
 
 public class Pagination {
 	
--- src/main/java/com/kh/spring/community/model/service/board/Board_ReplyServiceImpl.java
+++ src/main/java/com/kh/spring/community/model/service/board/Board_ReplyServiceImpl.java
@@ -1,16 +1,13 @@
 package com.kh.spring.community.model.service.board;
 
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.kh.spring.community.model.dao.board.Board_AttachmentDao;
 @Service
-public class Board_ReplyServiceImpl implements BoardService {
+public class Board_ReplyServiceImpl implements Board_ReplyService {
+/*
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 	
 	@Autowired
 	private Board_AttachmentDao board_AttachmentDao;
-	
+	*/
 }
