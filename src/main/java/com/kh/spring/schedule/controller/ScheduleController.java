f8de58028928e7cd7f66a00be2b19e418e890e3d
Luna230-L <lsy77877@naver.com>
2021-11-12 21:12:59 +0900

    211112 빌드업

--- src/main/java/com/kh/spring/schedule/controller/ScheduleController.java
+++ src/main/java/com/kh/spring/schedule/controller/ScheduleController.java
@@ -5,8 +5,11 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.w3c.dom.events.Event;
 
 import com.kh.spring.employee.model.vo.Employee;
 import com.kh.spring.member.model.vo.Member;
@@ -18,18 +21,28 @@ import com.kh.spring.schedule.model.vo.Schedule;
 public class ScheduleController {
 	@Autowired 
 	private ScheduleService scheduleService;
-
 	
-	@RequestMapping("main.sc")
-	public String scheduleMain(Model m, HttpServletRequest request) {
-		Member mem = (Member) request.getSession().getAttribute("loginUser");
-		Employee emp = (Employee) scheduleService.selectEmployee(mem);
-		m.addAttribute("mem", mem);
-		m.addAttribute("emp", emp);
-		System.out.println("emp : " + emp);
-		return "schedule/scheduleMain";
-	}
+	//리사이즈, 드롭(드래그)
 	
+	 
+	@RequestMapping("data.json")
+	  public String create(@RequestBody Event event) {
+	    // 생략
+	    return "schedule/scheduleMain";
+	  }
+	 
+//	 
+//	@ResponseBody
+//	@RequestMapping("main.sc")
+//	public String scheduleMain(Model m, HttpServletRequest request) {
+//		Member mem = (Member) request.getSession().getAttribute("loginUser");
+//		Employee emp = (Employee) scheduleService.selectEmployee(mem);
+//		m.addAttribute("mem", mem);
+//		m.addAttribute("emp", emp);
+//		System.out.println("emp : " + emp);
+//		return "schedule/scheduleMain";
+//	}
+//	
 	//일정등록
 	@ResponseBody
 	@RequestMapping(value = "insert.sc")
