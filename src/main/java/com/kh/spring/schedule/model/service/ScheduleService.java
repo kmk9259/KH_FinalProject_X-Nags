b787167a36fca5f2f76a6a4f3728bb4d4f0bd036
Luna230-L <lsy77877@naver.com>
2021-11-18 16:24:27 +0900

    211118 crud 완성

--- src/main/java/com/kh/spring/schedule/model/service/ScheduleService.java
+++ src/main/java/com/kh/spring/schedule/model/service/ScheduleService.java
@@ -9,13 +9,12 @@ import com.kh.spring.schedule.model.vo.Schedule;
 public interface ScheduleService {
 
 	int insertSchedule(Schedule sc);
+	
+	int updateSchedule(Schedule sc);
+
+	int deleteSchedule(Schedule sc);
 
 	ArrayList<Schedule> scheduleMain(Map<String,String> paramMap);
 
-//	Employee selectEmployee(Member mem);
-
-//	int updateSchedule(Schedule sc);
-//
-//	int deleteSchedule(Schedule sc);
 
 }
